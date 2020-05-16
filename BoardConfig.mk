40a75de6d4f5380052324f98e1f93cc46f60e1cf
faham1997 <nafidfaham08@gmail.com>
2020-05-17 03:57:34 +0600

    rolex: enable qcom time services

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,9 @@ VENDOR_SECURITY_PATCH := 2020-06-05
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+# Timeservice
+BOARD_USES_QC_TIME_SERVICES := true
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 BOARD_VNDK_VERSION := current
