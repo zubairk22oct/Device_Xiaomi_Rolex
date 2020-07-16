e9ceb32c044cce3d919d0f648d0860a8ac72aa28
zubairk22oct <areeb9670@gmail.com>
2020-07-16 06:10:52 +0000

    Pixel-Experience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/aosp_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rolex-user \
-    lineage_rolex-userdebug
+    aosp_rolex-user \
+    aosp_rolex-userdebug
