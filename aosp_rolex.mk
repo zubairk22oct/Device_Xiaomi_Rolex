18eb295f9c26b04d4ce26b85db0dcbfba974ce0a
zubairk22oct <areeb9670@gmail.com>
2020-07-16 07:08:59 +0000

    Add gapps target & enable google recorder

--- aosp_rolex.mk
+++ aosp_rolex.mk
@@ -20,8 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
