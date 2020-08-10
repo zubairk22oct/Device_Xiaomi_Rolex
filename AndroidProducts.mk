6dd5f8e60da97790cd35e32899bc1b6e20551522
zubairk22oct <areeb9670@gmail.com>
2020-08-10 06:29:49 +0000

    PE >>>> Corvus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rolex.mk
+    $(LOCAL_DIR)/du_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rolex-user \
-    aosp_rolex-userdebug
+    du_rolex-user \
+    du_rolex-userdebug
