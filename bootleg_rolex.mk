d0110b994a219e32a84b287ba7465235665d1933
zubairk22oct <areeb9670@gmail.com>
2020-11-11 07:28:47 +0000

    update to coral november build fp

--- bootleg_rolex.mk
+++ bootleg_rolex.mk
@@ -41,4 +41,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="rolex" \
     PRODUCT_NAME="rolex" \
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
