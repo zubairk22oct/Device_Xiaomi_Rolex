06c566cb54f4f375391a0c5959a4b335499a37f5
Foxtrot47 <37036622+Foxtrot47@users.noreply.github.com>
2020-11-19 10:36:15 +0530

    rolex: Build libhidltransport

--- device.mk
+++ device.mk
@@ -201,7 +201,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
+    libhidltransport.vendor \
+    libhidltransport
+    
 
 # HW crypto
 PRODUCT_PACKAGES += \
