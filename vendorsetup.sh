bb55c8f277d5c8242ea33cc916791cf06378d5b7
zubairk22oct <areeb9670@gmail.com>
2021-06-22 13:18:03 +0530

    Track Hals

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio hardware/qcom-caf/msm8996/audio
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display hardware/qcom-caf/msm8996/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media hardware/qcom-caf/msm8996/media
